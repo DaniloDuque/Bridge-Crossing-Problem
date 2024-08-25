f1da1ea027c7f1fd7dddb7ccfb191636ea27d9a2
Danilo Duque <duque@mcmini.local>
2024-08-25 14:10:49 -0600

    Makefile added

--- code/Main.c
+++ code/Main.c
@@ -3,11 +3,24 @@
 int mode, sz, k_i, k_j;
 double mu, t_si, t_sj, lbv, upv, ambProb;
 
+
+void Carnage(){
+
+}
+
+void Semaphore(){
+
+}
+
+void Traffic(){
+
+}
+
 int main(){
 
-    scanf("%d %d %Lf %Lf %Lf %Lf", &mode, &sz, &mu, &lbv, &upv, &ambProb);
+    scanf("%d %d %lf %lf %lf %lf", &mode, &sz, &mu, &lbv, &upv, &ambProb);
     if(mode==1) scanf("%d %d", &k_i, &k_j);
-    if(mode==2) scanf("%Lf %Lf", &t_si, &t_sj);
+    if(mode==2) scanf("%lf %lf", &t_si, &t_sj);
     (!mode)? Carnage() : (mode==1)? Semaphore() : Traffic();
     return 0;
 
