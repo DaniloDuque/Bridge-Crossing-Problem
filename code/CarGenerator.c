db5a7f2c5d94482ef1734ff7041bd5db1d57e55c
DaniloDuque <dduque@estudiantec.cr>
2024-08-25 17:11:41 -0600

    ...

--- code/CarGenerator.c
+++ code/CarGenerator.c
@@ -1,16 +1,17 @@
 #include "Car.c"
 
 typedef struct{
+    int id;
     double mu;
     double lbv;
-    double upv;
+    double ubv;
     double ambProb;
 } domain;
 
-void* CarGenerator(double mu, double l, double u, double p){
+void* CarGenerator(int id, double mu, double l, double u, double p){
     while(1){
         double w = -mu*log(1-prob());
+        printf("id:%d %lf\n", id, w);
         sleep(w);
-        printf("%lf\n", w);
     }
 }
--- code/Main.c
+++ code/Main.c
@@ -1,40 +1,39 @@
 #include "CarGenerator.c" 
+#include <pthread.h>
 
 int mode, sz, k_i, k_j;
-double mu, t_si, t_sj, lbv, upv, ambProb;
+double mu_i, mu_j, t_si, t_sj, lbv_i, ubv_i, lbv_j, ubv_j, ambProb_i, ambProb_j;
 
 void* run_car_generator(void* arg) {
-    domain* params = (domain*)arg;
-    CarGenerator(params->mu, params->lbv, params->upv, params->ambProb);
+    domain* dom = (domain*)arg;
+    CarGenerator(dom->id, dom->mu, dom->lbv, dom->ubv, dom->ambProb);
     return NULL;
 }
 
-void INIT(double m, double l, double u, double p) {
+void INIT(double m_i, double m_j, double l_i, double u_i, double l_j, double u_j , double p_i, double p_j) {
     pthread_t threads[2];
-    domain params = {m, l, u, p};
-    for (int i = 0; i < 2; i++)
-        if (pthread_create(&threads[i], NULL, run_car_generator, (void*)&params) != 0) {
-            fprintf(stderr, "Failed to create thread\n");
-            exit(EXIT_FAILURE);
-        }
+    domain lft = {0, m_i, l_i, u_i, p_i}, rght = {1, m_j, l_j, u_j, p_j};
+    pthread_create(&threads[0], NULL, run_car_generator, (void*)&lft);
+    pthread_create(&threads[1], NULL, run_car_generator, (void*)&rght);
     for (int i = 0; i < 2; i++) pthread_join(threads[i], NULL);
 }
 
 void Carnage() {
-    INIT(mu, lbv, upv, ambProb);
+    INIT(mu_i, mu_j, lbv_i, ubv_i, lbv_j, ubv_j, ambProb_i, ambProb_j);
 }
 
 void Semaphore() {
-    INIT(mu, lbv, upv, ambProb);
+    INIT(mu_i, mu_j, lbv_i, ubv_i, lbv_j, ubv_j, ambProb_i, ambProb_j);
 }
 
 void Traffic() {
-    INIT(mu, lbv, upv, ambProb);
+    INIT(mu_i, mu_j, lbv_i, ubv_i, lbv_j, ubv_j, ambProb_i, ambProb_j);
 }
 
 int main() {
     srand48(time(0));
-    scanf("%d%d%lf%lf%lf%lf", &mode, &sz, &mu, &lbv, &upv, &ambProb);
+    scanf("%d%d", &mode, &sz);
+    scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &mu_i, &mu_j, &lbv_i, &ubv_i, &lbv_j, &ubv_j, &ambProb_i, &ambProb_j);
     if (mode == 1) scanf("%d%d", &k_i, &k_j);
     if (mode == 2) scanf("%lf%lf", &t_si, &t_sj);
     (!mode)? Carnage() : (mode==1)? Semaphore() : Traffic();
