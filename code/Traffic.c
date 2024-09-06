03556862c89c299ec575d0a540c745162d52fcfb
DaniloDuque <dduque@estudiantec.cr>
2024-09-05 19:09:37 -0600

    Semaphore Ready, Traffic almost ready, deadlock when ambulance leaves bridge

--- code/Traffic.c
+++ code/Traffic.c
@@ -17,13 +17,13 @@ int id=0;
 void *run_Traffic() {
     while (1) {
         reset();
-        signal(&pass);
+        broadcast(&pass);
         while(cz->amb_waiting || (cz->t1>0)){
             if(!cz->dir && !cz->amb_waiting && !cz->bridge[0].frst && cz->bridge[cz->sz+1].frst) break;
             DEBUG(id++);
             wait(&empty, &bridge_mutex);
         }reset();
-        signal(&pass);
+        broadcast(&pass);
         while(cz->amb_waiting || (cz->t2>0)) {
             if(!cz->dir && !cz->amb_waiting && cz->bridge[0].frst && !cz->bridge[cz->sz+1].frst) break;
             DEBUG(id++);
@@ -37,13 +37,14 @@ void* CrossTrafficCar(void *arg) {
     int st = start(car), end = end(car);
     lock(&cz->bridge[st-car->dir].scnd); 
     cz->bridge[st-car->dir].frst=1; 
-    while(cz->amb_waiting || ((car->dir==1)? cz->t1<=0 : cz->t2<=0) || car->dir!=cz->sem) wait(&pass, &bridge_mutex), DEBUG(id++);
-    (car->dir == 1)? --cz->t1 : --cz->t2;
+    if(!cz->dir) broadcast(&empty);
+    while(cz->amb_waiting || ((car->dir==1)? cz->t1<=0 : cz->t2<=0) || (car->dir!=cz->sem)) wait(&pass, &bridge_mutex), DEBUG(id++);
+    (car->dir==1)? --cz->t1 : --cz->t2;
     cz->dir += car->dir;
     CrossBridge(car, st, end, 1);
     cz->dir -= car->dir;
     cz->bridge[end].frst = 0;
-    if (cz->dir == 0) signal(&empty);
+    if (!cz->dir) broadcast(&empty);
     unlock(&cz->bridge[end].scnd);
     free(car);
     return 0;
@@ -52,14 +53,15 @@ void* CrossTrafficCar(void *arg) {
 void *CrossTrafficAmbulance(void *arg){
     Car* amb = (Car*)arg;
     int st = start(amb), end = end(amb);
-    lock(&cz->bridge[st-amb->dir].scnd); ++cz->amb_waiting; 
-    cz->bridge[st-amb->dir].frst=2;
-    while((amb->dir==1)? cz->dir<0 : cz->dir>0) wait(&empty, &cz->bridge[st-amb->dir].scnd);
-    if(amb->dir == cz->sem) (amb->dir==1)? --cz->t1 : --cz->t2;
+    lock(&cz->bridge[st-amb->dir].scnd); 
+    ++cz->amb_waiting; cz->bridge[st-amb->dir].frst=2;
+    if(!cz->dir) broadcast(&empty);
+    while((amb->dir==1)? cz->dir<0 : cz->dir>0) wait(&empty, &bridge_mutex), DEBUG(id++);
+    if(amb->dir==cz->sem) (amb->dir==1)? cz->t1-- : cz->t2--;
     cz->dir+=amb->dir; --cz->amb_waiting;
     CrossBridge(amb, st, end, 2);
     cz->dir-=amb->dir; cz->bridge[end].frst=0;
-    if(cz->dir==0) signal(&empty);
+    if(!cz->dir) broadcast(&empty);
     unlock(&cz->bridge[end].scnd);
     free(amb);
     return 0;
