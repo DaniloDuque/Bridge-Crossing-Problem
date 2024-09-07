fe20537606dc56e330f8799e4432f71ec5b764fc
DaniloDuque <dduque@estudiantec.cr>
2024-09-07 12:25:12 -0600

    Main instructions

--- code/Main.c
+++ code/Main.c
@@ -1,11 +1,35 @@
 #include "Modes.c"
 
 int main() {
+
     srand48(time(0));
-    scanf("%d%d", &mode, &sz);
-    scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &mu_i, &mu_j, &lbv_i, &ubv_i, &lbv_j, &ubv_j, &p_i, &p_j);
-    if (mode == 1) scanf("%lf%lf", &t_si, &t_sj);
-    if (mode == 2) scanf("%d%d", &k_i, &k_j);
-    (!mode)? Carnage() : (mode==1)? Semaphore() : Traffic();
+    printf("Select Mode (0 for Carnage, 1 for Semaphore, 2 for Traffic Officer): ");
+    scanf("%d", &mode);
+    printf("Enter the size of the bridge (positive integer): ");
+    scanf("%d", &sz);
+    puts("Enter the average car spawn time in seconds (positive real number):");
+    printf("Left side: "); scanf("%lf", &mu_i); 
+    printf("Right side: "); scanf("%lf", &mu_j); 
+    puts("Enter the velocity range of cars (positive real numbers):");
+    printf("Left side (lower bound and upper bound): "); scanf("%lf%lf", &lbv_i, &ubv_i); 
+    printf("Right side (lower bound and upper bound): "); scanf("%lf%lf", &lbv_j, &ubv_j); 
+    puts("Enter the probability of an ambulance appearing (real number in [0, 1]):");
+    printf("Left side: "); scanf("%lf", &p_i);
+    printf("Right side: "); scanf("%lf", &p_j);
+
+    if (mode == 1) {
+        puts("Enter the duration of the semaphore in seconds (positive real number):");
+        printf("Left side: "); scanf("%lf", &t_si);
+        printf("Right side: "); scanf("%lf", &t_sj);
+    }
+
+    if (mode == 2) {
+        puts("Enter the number of cars allowed to pass per round (positive integer):");
+        printf("Left side: "); scanf("%d", &k_i);
+        printf("Right side: "); scanf("%d", &k_j);
+    }
+
+    (!mode) ? Carnage() : (mode == 1) ? Semaphore() : Traffic();
     return 0;
 }
+
